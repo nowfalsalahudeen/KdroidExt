f9ea1c8f2bb75d94eaf59beee57b12e94251702f
Nowfal <nowfalsalahudeen@gmail.com>
2019-05-26 13:16:55 +0530

    1.1.6 Merged some new ext

--- app/src/main/java/com/nowfal/kdroid/MainActivity.kt
+++ app/src/main/java/com/nowfal/kdroid/MainActivity.kt
@@ -4,6 +4,7 @@ import `in`.nowfal.kdroid.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nowfal.kdroidext.Utils.setImageUrl
+import com.nowfal.kdroidext.kotx.ext.app.broadcastLocal
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 setImageUrl("https://vtoqas3.s3.amazonaws.com/da9e3825132c34e5799e61fc08a22336_avatar_5c5568f1a87ba.png",iv)
+        this.broadcastLocal("ASd")
     }
 }
